541fff8eea5af135bf3d750f7ec3e92e3b90cde2
Riadh <riadh@ioasys.com.br>
2017-09-03 21:38:41 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-"# movies" 
+# Movie
+A sample project that show movies list from themoviesdb
 
+
+![Screenshots](https://raw.githubusercontent.com/riadhnet/movies-android/development/screenshots/1.png)
+
+
+## Getting Started
+
+This project doesn't need any build instructions : just sdk 26 and buildTools "6.0.1
+
+
+### Third-party libraries:
+
+-Android annotation : https://github.com/androidannotations/androidannotations
+this library simplify the android code , making it easy to read and maintain , specially helps in fast dev .
+
+
+-retrofit 2 :http://square.github.io/retrofit/
+A type-safe HTTP client for Android and Java
+
+-Gson : https://github.com/google/gson
+helps to convert Java Objects into JSON and back.
+
+-Glide :https://github.com/bumptech/glide
+An image loading and caching library for Android focused on smooth scrolling : used by google developers
+
+-Material circular progress view : https://github.com/rahatarmanahmed/CircularProgressView/
+
+-Material dialog : https://github.com/afollestad/material-dialogs
--- app/src/main/java/com/riadh/movies/utils/GlideUtils.java
+++ app/src/main/java/com/riadh/movies/utils/GlideUtils.java
@@ -2,48 +2,13 @@ package com.riadh.movies.utils;
 
 import android.content.Context;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.SimpleTarget;
 
-import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 public class GlideUtils {
 
-    public static void loadImageCropCircle(Object url, Context with, ImageView into) {
-        if (with == null) {
-            return;
-        }
-        try {
-            Glide.with(with).load(url)
-                    .bitmapTransform(new CropCircleTransformation(with))
-                    .dontAnimate()
-                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                    .into(into);
-        } catch (Exception ignored) {
-
-        }
-
-    }
-
-    public static void loadImageCropCircleIntoTextViewDrawable(Object url, Context with, final TextView into, int width, int height) {
-        if (with == null) {
-            return;
-        }
-        Glide.with(with)
-                .load(url)
-                .bitmapTransform(new CropCircleTransformation(with))
-                .into(new SimpleTarget<GlideDrawable>(width, height) {
-                    @Override
-                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
-                        into.setCompoundDrawablesWithIntrinsicBounds(resource, null, null, null);
-                    }
-                });
-    }
 
     public static void loadImage(Object url, Context with, ImageView into) {
         if (with == null) {
